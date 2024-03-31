54b8d396a3eb88192f237d70ca2e61ba0b94a133
Showan <44385680+showansimkhada@users.noreply.github.com>
2024-04-01 11:19:22 +1300

    fux/middleware

--- work-financial-trends/middleware.ts
+++ work-financial-trends/middleware.ts
@@ -3,7 +3,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  const protectedPaths = ["/dashBS", "/dashMO", "/bs", "/mo", "/profile"];
+  const protectedPaths = ["/dash", "report", "/bs", "/mo", "/profile"];
   const isPathProtected = protectedPaths?.some((path) => pathname == path);
   const res = NextResponse.next();
   const token = await getToken({ req });
