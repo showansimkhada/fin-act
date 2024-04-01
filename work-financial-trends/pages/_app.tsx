dc292c19f6953f45bcac79f40b7afb8242e20412
Showan <showansimkhada@hotmail.com>
2024-04-01 14:23:41 +1300

    speed-insight

--- work-financial-trends/pages/_app.tsx
+++ work-financial-trends/pages/_app.tsx
@@ -4,10 +4,13 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import { Providers } from '@/lib/providers'
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <Providers>
       <SessionProvider session={pageProps.session}>
+        <SpeedInsights/>
         <Component {...pageProps} />
       </SessionProvider>
     </Providers>
