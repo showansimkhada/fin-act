690c79ce9144cf41b724c87227e6f7b494d7a89e
Showan <showansimkhada@hotmail.com>
2024-06-03 10:14:50 +1200

    feat/font awesome added

--- work-financial-trends/pages/_app.tsx
+++ work-financial-trends/pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import { Providers } from '@/lib/providers'
 
+// Fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit)
+
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
