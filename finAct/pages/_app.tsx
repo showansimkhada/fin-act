0fcb94257c6c36ba2b701555b2a23deef2feacf6
Showan <showansimkhada@hotmail.com>
2024-08-04 19:21:09 +1200

    packages updated

--- finAct/pages/_app.tsx
+++ finAct/pages/_app.tsx
@@ -9,13 +9,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 library.add(faTrash, faEdit)
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <Providers>
       <SessionProvider session={pageProps.session}>
-        <SpeedInsights/>
         <Component {...pageProps} />
       </SessionProvider>
     </Providers>
