5692524202204f241719445fc3a0955e39fd83d0
Showan <showansimkhada@hotmail.com>
2024-08-05 19:59:42 +1200

    added connection for mongodb

---
+++ finAct_V2/connection/connection.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
+const URI = process.env.MONGO_URI;
+module.exports = mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true});
