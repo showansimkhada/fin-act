53d9327afd900bda503ba545f7e7576c72bbcad5
Showan <44385680+showansimkhada@users.noreply.github.com>
2024-11-03 14:10:59 +1300

    edited next config file

--- finAct/next.config.mjs
+++ finAct/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  experimental: {
-    esmExternals: "loose",
-    serverComponentsExternalPackages: ["mongoose"],
-  },
+  // swcMinify: false,
+  // experimental: {
+  //   esmExternals: "loose",
+  //   serverComponentsExternalPackages: ["mongoose"],
+  // },
   webpack: (config) => {
     config.experiments = {
       topLevelAwait: true,
