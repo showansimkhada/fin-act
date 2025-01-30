9328e0a4e4cbd774716b9c36d5b5e9f3f686e283
Showan <44385680+showansimkhada@users.noreply.github.com>
2025-01-31 12:19:09 +1300

    removed console log and change return type to string

--- finAct/lib/funcPage.ts
+++ finAct/lib/funcPage.ts
@@ -49,8 +49,7 @@ export function getYear(date: string) {
 // Separate the string and create array then add all the items from array
 export function sumAmt(str: string) {
   let x = str.split(',')
-  let sum = 0
-  console.log(x)
+  let sum = 0;
   for (let i = 0; i < x.length; i++) {
     // check if it got other expression
     if (!x[i].match(/^([0-9]{1,})?(\.)?([0-9]{1,})?$/) || !x[i]) {
@@ -58,7 +57,7 @@ export function sumAmt(str: string) {
       sum += parseFloat(x[i])
     }
   }
-  return sum;
+  return sum.toString();
 }
 
 // Convert the string into addition expression
