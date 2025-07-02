33797bc70a320ff477aede149b9033e253c4a342
Showan Simkhada <showan@Showans-MacBook-Pro.local>
2025-07-02 22:22:15 +1200

    removed all old files

--- finAct_V1/connection/connection.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-const URI = process.env.MONGO_URI;
-module.exports = mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true});
--- finAct_V2/connection/connection.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
-
-const URI = process.env.MONGO_URI;
-module.exports = mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true});
