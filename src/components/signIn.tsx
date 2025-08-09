5d4868b001ffc1be52cc4cccadbe3afbfc16fe40
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-09 23:47:34 +1200

    Applied styles and updated message for errors during signin

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -7,60 +7,53 @@ import Image from 'next/image'
 import logo from "@/public/logo.svg"
 
 export default function SignIn() {
-  const [user, setUser] = useState('')
-  const [pass, setPass] = useState('')
-  const [mess, setMess] = useState('')
-  const router = useRouter();
-  const dispatch = useDispatch()
-  var callBackUrl = "/dash";
+	const [user, setUser] = useState('')
+	const [pass, setPass] = useState('')
+	const [mess, setMess] = useState('')
+	const router = useRouter();
+	const dispatch = useDispatch()
+	var callBackUrl = "/dash";
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if (!user && !pass) {
-      setMess("Please enter username and password")
-    }
-    else if (!user) {
-      setMess("Plese enter username")
-    }
-    else if (!pass) {
-      setMess("Please enter password")
-    } else {
-      const result = await signIn("credentials", {
-        username: user,
-        password: pass,
-        redirect: false
-      })
-      if (result?.error) {
-        setMess(result.error)
-      } else {
-        dispatch(dataSlice.actions.setUserName(user))
-        router.push(callBackUrl);
-      }
-    }
-  }
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (!user && !pass) {
+			setMess("Invalid user and password!")
+		} else {
+			const result = await signIn("credentials", {
+				username: user,
+				password: pass,
+				redirect: false
+			})
+			if (result?.error) {
+				setMess(result.error)
+			} else {
+				dispatch(dataSlice.actions.setUserName(user))
+				router.push(callBackUrl);
+			}
+		}
+	}
 
-  return (
-    <div className="d-flex align-items-center py-4 bg-body-tertiary text-center">
-      <div className="form-signin w-100 m-auto">
-        <form onSubmit={handleSubmit}>
-          <Image className='mb-4' src={logo} alt="logo" width={55} height={55} />
-          <h1>Please Signin</h1>
-          <div className="form-floating">
-            <input className="form-control" type="text" id="username" name="username" autoCapitalize='none' onChange={(event)=> {
-              setUser(event.target.value)
-            }}></input>
-            <label htmlFor="username">Username</label>
-          </div>
-          <div className='form-floating'>
-            <input className="form-control" type="password" id="password" name="password" onChange={(event) => {
-              setPass(event.target.value)
-            }}></input>
-            <label htmlFor='password'>Password</label>
-          </div>
-          <input className="btn btn-primary w-100" type="submit" value={`Sign In`}></input>
-          <p><code>{mess}</code></p>
-        </form>
-      </div>
-    </div>
-  )
+	return (
+		<div className="form-signin">
+			<Image className='react-logo' src={logo} alt="logo"/>
+			<form onSubmit={handleSubmit}>
+				<div className="form-floating">
+					<input className="form-control" type="text" id="username" name="username" autoCapitalize='none' onChange={(event)=> {
+					setUser(event.target.value)
+					}}></input>
+					<label htmlFor="username">Username</label>
+				</div>
+				<div className='form-floating'>
+					<input className="form-control" type="password" id="password" name="password" onChange={(event) => {
+					setPass(event.target.value)
+					}}></input>
+					<label htmlFor='password'>Password</label>
+				</div>
+				<input className="btn" type="submit" value={`Sign In`}></input>
+				<p>
+					{mess}
+				</p>
+			</form>
+		</div>
+	)
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,55 +6,55 @@ import Users from "@/lib/utils/models/userModel"
 import bcrypt from 'bcrypt'
 
 if (!process.env.NEXTAUTH_SECRET) {
-  throw new Error(
-    "please provide process.env.NEXTAUTH_SECRET environment variable"
-  );
+	throw new Error(
+		"please provide process.env.NEXTAUTH_SECRET environment variable"
+	);
 }
 
 export const authOptions: NextAuthOptions = ({
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "username", type: "text" },
-        password: { label: "password", type: "password" }
-      },
-      async authorize(credentials) {
-        await dbConnect()
-        const username = credentials?.username;
-        const pass = credentials?.password;
-        const findUser = await Users.findOne({username: username})
-        if (findUser && bcrypt.compareSync(pass || "", findUser.password)) {
-          return findUser
-        } else {
-          throw new Error ("Incorrect username and password!!")
-        }
-      }
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.data = user
-        return token
-      }
-      return token
-    },
-    async session({ session, token }) {
-      return {
-        ...session,
-        user: {
-          ...token
-        }
-      }
-    }
-  },
-  session: {
-    strategy: "jwt"
-  },
-  pages: {
-    signIn: '/',
-  }
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				username: { label: "username", type: "text" },
+				password: { label: "password", type: "password" }
+			},
+			async authorize(credentials) {
+				await dbConnect()
+				const username = credentials?.username;
+				const pass = credentials?.password;
+				const findUser = await Users.findOne({username: username})
+				if (findUser && bcrypt.compareSync(pass || "", findUser.password)) {
+					return findUser
+				} else {
+					throw new Error ("Invalid user and password!")
+				}
+			}
+		}),
+  	],
+	callbacks: {
+		async jwt({ token, user }) {
+		if (user) {
+			token.data = user
+			return token
+		}
+		return token
+		},
+		async session({ session, token }) {
+		return {
+			...session,
+			user: {
+			...token
+			}
+		}
+		}
+	},
+	session: {
+		strategy: "jwt"
+	},
+	pages: {
+		signIn: '/',
+	}
 });
 
 export default NextAuth(authOptions)
