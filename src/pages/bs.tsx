3e1b82a00b03846d4842b635df33d2dfe5e8e5f6
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-14 19:06:21 +1200

    Renamed the reference components

--- src/pages/bs.tsx
+++ src/pages/bs.tsx
@@ -1,4 +1,4 @@
-import Navbars from "@/components/navBar"
+import Navbars from "@/components/nav-bar"
 
 import BS, { IBS } from '@/lib/utils/models/bsModel'
 import User, { IUSER } from '@/lib/utils/models/userModel'
@@ -12,7 +12,6 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
 
 type Props = {
     bsData: IBS[] ,
@@ -35,7 +34,7 @@ export default function( {bsData, userData}: Props) {
     const dataUser = userData.filter((x) => {
         return x.username === username
     })
-    const [data, setData] = useState(dataBS);
+    const [data] = useState(dataBS);
     const fweTotal = dataBS.reduce((c, x) => c + Number(x.fWI), 0).toFixed(2);
     const sweTotal = dataBS.reduce((c, x) => c + Number(x.sWI), 0).toFixed(2);
     const wspTotal = dataBS.reduce((c, x) => c + Number(x.weeklySpent), 0).toFixed(2);
@@ -45,57 +44,59 @@ export default function( {bsData, userData}: Props) {
     })
 
     const dataTable = (
-            <Table striped bordered id="bsOutput" responsive="sm" className="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>{isClient? dataUser[0]?.firstname: ''}'s WI</th>
-                        <th>{isClient? dataUser[0]?.sfirstname: ''}'s WI</th>
-                        <th>Return</th>
-                        <th>Opening Balance</th>
-                        <th>Closing Balance</th>
-                        <th>Weekly Spent</th>
-                        <th>Weekly Save</th>
-                    </tr>
-                </thead>
-                <tbody id='data'>
-                    {isClient? data.map((x) => (
-                      <tr key={formatDate(x.year.toString() + '/' + x.month.toString() + '/' + x.date.toString(), 1)}>
-                      <td>{formatDate(x.year.toString() + '/' + x.month.toString() + '/' + x.date.toString(), 1)}</td>
-                      <td>{x.fWI.toString()}</td>
-                      <td>{x.sWI.toString()}</td>
-                      <td>{x.return.toString()}</td>
-                      <td>{x.openingBalance.toString()}</td>
-                      <td>{x.closingBalance.toString()}</td>
-                      <td>{x.weeklySpent.toString()}</td>
-                      <td className="d-flex flex-row justify-content-between">{x.weeklySave.toString()}</td>
-                  </tr>
-                    )): ''}
-                </tbody>
-                <tfoot id='total'>
-                    {isClient ? 
-                    <tr>
-                        <td>{'Total'}</td>
-                        <td>{fweTotal}</td>
-                        <td>{sweTotal}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>{wspTotal}</td>
-                        <td>{wsaTotal}</td>
-                    </tr>
-                    : ''}
-                </tfoot>
-            </Table>
+		<>
+			<div className="bs-container">
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>{isClient? dataUser[0]?.firstname: ''}'s WI</th>
+							<th>{isClient? dataUser[0]?.sfirstname: ''}'s WI</th>
+							<th>Return</th>
+							<th>Opening Balance</th>
+							<th>Closing Balance</th>
+							<th>Weekly Spent</th>
+							<th>Weekly Save</th>
+						</tr>
+					</thead>
+					<tbody>
+						{isClient? data.map((x) => (
+							<tr key={formatDate(x.year.toString() + '/' + x.month.toString() + '/' + x.date.toString(), 1)}>
+								<td>{formatDate(x.year.toString() + '/' + x.month.toString() + '/' + x.date.toString(), 1)}</td>
+								<td>{x.fWI.toString()}</td>
+								<td>{x.sWI.toString()}</td>
+								<td>{x.return.toString()}</td>
+								<td>{x.openingBalance.toString()}</td>
+								<td>{x.closingBalance.toString()}</td>
+								<td>{x.weeklySpent.toString()}</td>
+								<td>{x.weeklySave.toString()}</td>
+							</tr>
+                   		 )): ''}
+					</tbody>
+					<tfoot>
+						{isClient ? 
+							<tr>
+								<td>{'Total'}</td>
+								<td>{fweTotal}</td>
+								<td>{sweTotal}</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>{wspTotal}</td>
+								<td>{wsaTotal}</td>
+							</tr>
+						: ''}
+					</tfoot>
+				</table>
+			</div>
+		</>
     )
 
     return (
-        <div className='container-fluid'>
+		<>
             <Navbars/>
-            <div className="d-flex flex-row justify-content-between table-responsive-sm" style={{marginTop: '55px'}}>
-            </div>
             {dataTable}
-        </div>
+        </>
     )
 }
 
@@ -104,13 +105,17 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
     /* find all the data in our database */
     const findBS = await BS.find({})
     const data = findBS.sort((a, b) => {
-        return new Date(a.year.toString() + '-' + a.month.toString() + '-' + a.date.toString()).getTime() - new Date(b.year.toString() + '-' + b.month.toString() + '-' + b.date.toString()).getTime()
+        return new Date(
+			a.year.toString() + '-' + a.month.toString() + '-'
+			+ a.date.toString()).getTime() - new Date(b.year.toString()
+			+ '-' + b.month.toString() + '-' + b.date.toString()
+		).getTime()
     });
   
     /* Ensures all objectIds and nested objectIds are serialized as JSON data */
     const bsData = data.map((doc) => {
-      const pet = JSON.parse(JSON.stringify(doc))
-      return pet
+		const pet = JSON.parse(JSON.stringify(doc))
+		return pet
     })
 
     /* find all the data in our database */
@@ -118,8 +123,8 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
   
     /* Ensures all objectIds and nested objectIds are serialized as JSON data */
     const userData = results.map((doc) => {
-      const pet = JSON.parse(JSON.stringify(doc))
-      return pet
+		const pet = JSON.parse(JSON.stringify(doc))
+		return pet
     })
   
     return { props: { bsData: bsData, userData: userData } }
