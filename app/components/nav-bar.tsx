08f61e7a0bbbace3bc5503f336151550a91fdda0
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-18 22:51:35 +1200

    complete navbar and navlinks

---
+++ app/components/nav-bar.tsx
@@ -0,0 +1,26 @@
+import NavLinks from '@/app/components/nav-links';
+// import { PowerIcon } from '@heroicons/react/24/outline';
+import { signOut } from '@/auth';
+ 
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <input type='checkbox' id='nav-toggle'/>
+      <label htmlFor='nav-toggle' className='hamburger'>
+        &#9776;
+      </label>
+      <NavLinks />
+      <form
+        action={async () => {
+          'use server';
+          await signOut({ redirectTo: '/' });
+        }}
+      >
+        <button className='navbar-links'>
+          {/* <PowerIcon className="w-6" /> */}
+          <div>Sign Out</div>
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ app/components/nav-links.tsx
@@ -0,0 +1,59 @@
+'use client';
+ 
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+  UserIcon
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+  { 
+    name: 'Dashboard', 
+    href: '/dashboard',
+    class: "navbar-banner",
+    icon: HomeIcon 
+  },
+  {
+    name: 'Balance Sheet',
+    href: '/balancesheet',
+    class: "navbar-links",
+    icon: DocumentDuplicateIcon,
+  },
+  { 
+    name: 'Report', 
+    href: '/report',
+    class: "navbar-links",
+    icon: UserGroupIcon 
+  },
+  {
+    name: 'Profile',
+    href: '/profile',
+    class: "navbar-links",
+    icon: UserIcon
+  },
+];
+ 
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={link.class}
+          >
+            {/* <LinkIcon className="w-6" /> */}
+            <p className="">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
