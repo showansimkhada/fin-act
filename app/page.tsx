81263fc7318daabb7ebce9d8313e0fea6ee75056
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-18 14:02:14 +1200

    removed redux and changed the structure of the project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import AcmeLogo from '@/app/ui/acme-logo';
+// import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Login from './ui/loginin-form';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+        <Login/>
+    </main>
+  );
+}
