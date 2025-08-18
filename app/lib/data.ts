a7ae24ae058996ce7aa80d57673b1c435d622e8f
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-18 20:34:14 +1200

    Used MongoDB and implemented functions to fetch data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,37 +1,48 @@
 import { MongoClient } from 'mongodb';
-import {
-  User,
-  BalanceSheet,
-} from './definitions';
+import { User, IBS } from '@/app/lib/definitions'
 
 const MONGODB_URI = process.env.MONGO_URI!
 const client = new MongoClient(MONGODB_URI)
 
-export async function fetchUser(user: string) {
-    console.log('At /lib/data.ts fetchBS')
+export async function fetchUser(user: string): Promise<User | null> {
   try {
     await client.connect()
     const database = client.db(process.env.DB_NAME!)
     const collection = database.collection(process.env.USER_COLLECTION_NAME!);
-    // Pass username to fiter the database
-    const data = await collection.find({username: user}).toArray()
-    return data;
+    const data = await collection.findOne({username: user})
+    return data as User;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch user data');
+  } finally {
+    await client.close();
+  }
+}
+
+export async function fetchBS(user: string): Promise<IBS[] | null> {
+  try {
+    await client.connect();
+    const database = client.db(process.env.DB_NAME!);
+    const collection = database.collection(process.env.BS_COLLECTION_NAME!);
+    
+    const data = await collection.find({username: user}).toArray();
+    return data as IBS[];
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch Balance Sheet data');
   } finally {
     await client.close()
   }
 }
 
-export async function fetchBS(user: string) {
+export async function fetchLastBS(user: string): Promise<IBS | null> {
   try {
-    await client.connect()
-    const database = client.db(process.env.DB_NAME!)
+    await client.connect();
+    const database = client.db(process.env.DB_NAME!);
     const collection = database.collection(process.env.BS_COLLECTION_NAME!);
-    // Pass username to fiter the database
-    const data = await collection.find({username: user}).toArray()
-    return data;
+    
+    const data = await collection.find({username: user}).toArray();
+    return data[0] as IBS;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch Balance Sheet data');
