00498e57cd6fa39fcc961cee9ba4d73d812141ed
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-19 18:05:04 +1200

    added button element

---
+++ app/ui/button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className='btn'
+    >
+      {children}
+    </button>
+  );
+}
