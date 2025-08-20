575f16088ba9baa1f0fcdc681861e3005ad4c700
Showan Simkhada <brutalthreat@kali-mac.localhost>
2025-08-20 13:17:14 +1200

    moved button to components and login to forms

--- app/forms/login.tsx
+++ app/forms/login.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/app/ui/button';
+import { Button } from '@/app/components/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
