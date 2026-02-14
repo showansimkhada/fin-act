669ab987e4f5fb9e572894960a31b8e27134b1af
Showan <showansimkhada@hotmail.com>
2026-02-15 12:51:32 +1300

    Updated packages

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.experiments = {
-      topLevelAwait: true,
-      layers: true
-    };
-    return config;
-  }
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     topLevelAwait: true,
+  //     layers: true,
+  //   };
+  //   return config;
+  // }
 }
 
 export default nextConfig;
